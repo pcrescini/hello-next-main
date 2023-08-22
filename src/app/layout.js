dd2aebd36c50ff76d6ec0790060aeaced7962e63
Paul Crescini <pcrescini@gmail.com>
2023-08-22 08:13:56 -0500

    updated - updated layout to dynamic rendering

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,12 +2,16 @@ import React from 'react';
 
 import './styles.css';
 
+export const dynamic = 'force-dynamic';
+
 function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
         {children}
-      <footer> &copy; 2023 Paul C Crescini</footer>
+      <footer> &copy; 2023 Paul C Crescini - Page rendered on{' '}
+      {new Date().toLocaleString()}
+      </footer>
       </body>
     </html>
   );
